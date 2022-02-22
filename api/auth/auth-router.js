19e53a2d6566e18645d43bdda7c3db69457db0d0
James Blacklock <jamesblacklock@protonmail.com>
2022-02-22 12:40:15 -0500

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 const router = require('express').Router()
 const User = require('../users/users-model.js')
@@ -26,7 +27,13 @@ router.post('/login', (req, res, next) => {
   User.findBy({ username })
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome back ${user.username}...` })
+        const token = generateToken(user);
+
+
+        res.status(200).json({
+          message: `Welcome back ${user.username}...`,
+          token,
+        })
       } else {
         next({ status: 401, message: 'Invalid Credentials' })
       }
