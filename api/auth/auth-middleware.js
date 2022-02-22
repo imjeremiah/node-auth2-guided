74b9fa4e58cf861664cc4d236e4fd258c7d9dc97
James Blacklock <jamesblacklock@protonmail.com>
2022-02-22 13:15:59 -0500

    committing to "lecture"

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,8 +1,11 @@
 // AUTHENTICATION
 const restricted = (req, res, next) => {
   const token = req.headers.authorization
-  console.log(token);
-  next()
+  if(!token) {
+    next({ status: 401, message: 'access denied' });
+  } else {
+    next()
+  }
 }
 
 // AUTHORIZATION
