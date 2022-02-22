40af7d1b7ff26707dcbde56ee0a74adaa9267c7d
James Blacklock <jamesblacklock@protonmail.com>
2022-02-22 13:19:09 -0500

    committing to "lecture"

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,9 +1,17 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../../config');
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
   const token = req.headers.authorization
   if(!token) {
     next({ status: 401, message: 'access denied' });
   } else {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if(err) {
+        next({ status: 401, message: 'access denied' });
+      }
+    })
     next()
   }
 }
